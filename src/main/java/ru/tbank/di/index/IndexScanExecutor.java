59a7864534bf94be40fd9e53b5861e0b6dd746d0
e.gulyakin <e.gulyakin@tbank.ru>
2025-11-11 09:17:02 +0300

    Семинар 7

---
+++ src/main/java/ru/tbank/di/index/IndexScanExecutor.java
@@ -0,0 +1,21 @@
+package ru.tbank.di.index;
+
+import ru.tbank.di.execution.executor.Executor;
+
+public class IndexScanExecutor implements Executor {
+
+    @Override
+    public void open() {
+
+    }
+
+    @Override
+    public Object next() {
+        return null;
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
